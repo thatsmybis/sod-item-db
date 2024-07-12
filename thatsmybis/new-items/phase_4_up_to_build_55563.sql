72ef957c89f3eb99be16a123f0e4f524723f83bb
Lemmings19 <lemmings19@gmail.com>
2024-07-11 18:35:25 -0700

    Added SoD phase 4 items.

---
+++ thatsmybis/new-items/phase_4_up_to_build_55563.sql
@@ -0,0 +1,30 @@
+-- Contains items from builds in 1.15.3 55515, 55563.
+INSERT INTO `items`
+    (`item_id`, `parent_id`, `parent_item_id`, `expansion_id`, `faction`, `name`, `weight`, `quality`, `is_heroic`, `display_id`, `inventory_type`, `allowable_class`, `item_level`, `required_level`, `set_id`, `name_cn`, `name_de`, `name_es`, `name_fr`, `name_it`, `name_ko`, `name_pt`, `name_ru`, `created_at`)
+VALUES
+-- Felstriker was apparently in last build but for some reason was missing from my migrations.
+(228757, null, null, 4, null, "Felstriker",                          0.1, 1, 0, 0, 0, -1, 0, 58,  0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228747, null, null, 4, null, "Reins of the Golden Sabercat",        0.1, 4, 0, 0, 0, -1, 60, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228746, null, null, 4, null, "Fluorescent Green Mechanostrider",    0.1, 4, 0, 0, 0, -1, 60, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228748, null, null, 4, null, "Whistle of the Mottled Blood Raptor", 0.1, 4, 0, 0, 0, -1, 60, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228922, null, null, 4, null, "Staff of Dominance",                  0.1, 4, 0, 0, 0, -1, 71, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(227688, null, null, 4, null, "Gorehowl",                            0.1, 1, 0, 0, 0, -1, 1,   0, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228955, null, null, 4, null, "Ancient Cornerstone Grimoire",        0.1, 4, 0, 0, 0, -1, 60, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228912, null, null, 4, null, "Artifact Storage Key",                0.1, 1, 0, 0, 0, -1, 1,   0, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228929, null, null, 4, null, "Tempered Dark Iron Boots",            0.1, 4, 0, 0, 0, -1, 71, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228924, null, null, 4, null, "Tempered Dark Iron Boots",            0.1, 4, 0, 0, 0, -1, 71, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228927, null, null, 4, null, "Tempered Dark Iron Boots",            0.1, 4, 0, 0, 0, -1, 71, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228926, null, null, 4, null, "Tempered Dark Iron Boots",            0.1, 4, 0, 0, 0, -1, 71, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228925, null, null, 4, null, "Tempered Dark Iron Boots",            0.1, 4, 0, 0, 0, -1, 71, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00"),
+(228928, null, null, 4, null, "Tempered Dark Iron Boots",            0.1, 4, 0, 0, 0, -1, 71, 60, 0, "", "", "", "", "", "", "", "", "2024-07-10 00:00:00");
+INSERT INTO `items`
+    (`item_id`, `parent_id`, `parent_item_id`, `expansion_id`, `faction`, `name`, `weight`, `quality`, `is_heroic`, `display_id`, `inventory_type`, `allowable_class`, `item_level`, `required_level`, `set_id`, `name_cn`, `name_de`, `name_es`, `name_fr`, `name_it`, `name_ko`, `name_pt`, `name_ru`, `created_at`)
+VALUES
+(228993, null, null, 4, null, "Pattern: Bottomless Bag", 0.1, 3, 0, 0, 0, -1, 62, 0, 0, "", "", "", "", "", "", "", "", "2024-07-11 00:00:00"),
+(228994, null, null, 4, null, "Bottomless Bag",          0.1, 3, 0, 0, 0, -1, 62, 0, 0, "", "", "", "", "", "", "", "", "2024-07-11 00:00:00"),
+(228992, null, null, 4, null, "Onyxia Hide Backpack",    0.1, 2, 0, 0, 0, -1, 55, 0, 0, "", "", "", "", "", "", "", "", "2024-07-11 00:00:00"),
+(227929, null, null, 4, null, "Private Message",         0.1, 1, 0, 0, 0, -1,  1, 0, 0, "", "", "", "", "", "", "", "", "2024-07-11 00:00:00"),
+(225687, null, null, 4, null, "Rune of Infernal Armor",  0.1, 2, 0, 0, 0, -1, 60, 0, 0, "", "", "", "", "", "", "", "", "2024-07-11 00:00:00"),
+(225688, null, null, 4, null, "Rune of Mark of Chaos",   0.1, 2, 0, 0, 0, -1, 60, 0, 0, "", "", "", "", "", "", "", "", "2024-07-11 00:00:00"),
+(225690, null, null, 4, null, "Spell Notes: Frozen Orb", 0.1, 2, 0, 0, 0, -1, 60, 0, 0, "", "", "", "", "", "", "", "", "2024-07-11 00:00:00"),
+(225691, null, null, 4, null, "Spell Notes: Overheat",   0.1, 2, 0, 0, 0, -1, 60, 0, 0, "", "", "", "", "", "", "", "", "2024-07-11 00:00:00");
